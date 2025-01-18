68efb76d2e89d37e7e15592505e8a87fbff35aad
Tanvir <5takarcoin@gmail.com>
2025-01-19 00:05:48 +0600

    howa uchit

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express! v1   - logout issue");
+  res.send("Hello, TypeScript with Express! v1  howa uchit");
 });
 
 app.use("/api/v1", router);
